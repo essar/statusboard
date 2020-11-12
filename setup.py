51fedba816421cbf6cfcb806e87e2b35c681b164
Steve Roberts <steve.roberts@essarsoftware.co.uk>
2020-11-12 16:20:39 +0000

    Created setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='StatusBoard',
+      version='0.1',
+      description='StatusBoard library',
+      author='Steve Roberts',
+      author_email='steve.roberts <a> essarsoftware.co.uk',
+      url='https://www.essarsoftware.co.uk/projects/statusboard',
+      packages=['board', 'ic']
+      )
